283aa99390dfa572eee2c4d6d3c16a7ac1abc5a0
shihaoluoling <441168597@qq.com>
2020-09-04 14:03:40 +0800

    9.4

--- second-user-center/src/main/java/com/example/user/center/controller/ProductController.java
+++ second-user-center/src/main/java/com/example/user/center/controller/ProductController.java
@@ -4,6 +4,8 @@ package com.example.user.center.controller;
 import com.example.springbootdemoentity.entity.Product;
 import com.example.user.center.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +23,19 @@ public class ProductController {
     private ProductService productService;
 
 
-    @RequestMapping(value = "getcort")
+    @RequestMapping(value = "/getcort")
+//    @PreAuthorize("hasPermission('/getcort','r')")
     public String getProduct() {
-        String str =  productService.getProduct();
-        return str;
+//        String str =  productService.getProduct();
+        return "7878";
+    }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+        String a = bcryptPasswordEncoder.encode("123456");
+        System.out.println(a);
+//                BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+       boolean b = bcryptPasswordEncoder.matches("123456",a);
+        System.out.println(b);
     }
 }
