c5808f0926e75f192a78fe5e1b3de54db9ab4967
shihaoluoling <441168597@qq.com>
2020-09-09 16:14:18 +0800

    9.4

---
+++ second-payment-center/src/main/java/com/example/payment/center/controller/MiniPaymentOrderController.java
@@ -0,0 +1,21 @@
+package com.example.payment.center.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shihao
+ * @Title: MiniPaymentOrderController
+ * @ProjectName Second-order-center
+ * @Description: 小程序微信支付
+ * @date Created in
+ * @Version: $
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/payment/")
+@Api
+public class MiniPaymentOrderController {
+}
