af70416fda4babfaad8e3627ec7d7f2de5e64d66
shihaoluoling <441168597@qq.com>
2020-10-23 15:49:38 +0800

    10.23

--- second-payment-center/src/main/java/com/example/payment/center/controller/WithdrawalController.java
+++ second-payment-center/src/main/java/com/example/payment/center/controller/WithdrawalController.java
@@ -324,9 +324,9 @@ public class WithdrawalController {
             System.out.println(secondWithdrawal.getId());
             secondWithdrawal.setWithdrawalState(WithdrawalEnum.WithdrawalState.getWithdrawalState(type).getWithdrawalState());
             secondWithdrawalMapper.updateByPrimaryKeySelective(secondWithdrawal);
-            System.out.println(secondWithdrawals.get(0).getWithdrawalType());
+            System.out.println(secondWithdrawals.get(0).getSource());
             System.out.println(type);
-            if (secondWithdrawals.get(0).getWithdrawalType().equals(WithdrawalEnum.WithdrawalSource.USER.getWithdrawalSource())
+            if (secondWithdrawals.get(0).getSource().equals(WithdrawalEnum.WithdrawalSource.USER.getWithdrawalSource())
             && type.equals(WithdrawalEnum.WithdrawalState.COMPLETE.getWithdrawalState())){
                 //如果提现人是用户给子站点加余额
                 System.out.println("返现");
