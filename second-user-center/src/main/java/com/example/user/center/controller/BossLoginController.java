e18a81575062e50adea4767888c4d2955c23409a
shihaoluoling <441168597@qq.com>
2020-11-10 18:14:25 +0800

    11.9

--- second-user-center/src/main/java/com/example/user/center/controller/BossLoginController.java
+++ second-user-center/src/main/java/com/example/user/center/controller/BossLoginController.java
@@ -194,14 +194,6 @@ public class BossLoginController {
     }
     @RequestMapping(path = "/advertising", method = RequestMethod.POST)
     @ApiOperation(value = "添加广告图", notes = "添加商家基础信息")
-    @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "query", name = "service", value = "客服", required = false, type = "String"),
-            @ApiImplicitParam(paramType = "query", name = "weChat", value = "微信", required = false, type = "String"),
-            @ApiImplicitParam(paramType = "query", name = "newUserIntegral", value = "新用户认证加的积分", required = false, type = "Integer"),
-            @ApiImplicitParam(paramType = "query", name = "storeWithdrawalCommission", value = "商家提现手续费率", required = false, type = "Double"),
-            @ApiImplicitParam(paramType = "query", name = "userWithdrawalCommission", value = "用户提现手续费率", required = false, type = "Double"),
-            @ApiImplicitParam(paramType = "query", name = "sonWithdrawalCommission", value = "子站点提现手续费率", required = false, type = "Double"),
-    })
     @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public ResponseEntity<JSONObject> advertising(
             @RequestParam(value = "slideshow", required = false) String slideshow,
