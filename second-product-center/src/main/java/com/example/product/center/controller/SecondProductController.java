00f277d15e61e69b8b5bc2dcb5e86ce94b3a5a3f
shihaoluoling <441168597@qq.com>
2020-11-13 12:40:33 +0800

    11.12

--- second-product-center/src/main/java/com/example/product/center/controller/SecondProductController.java
+++ second-product-center/src/main/java/com/example/product/center/controller/SecondProductController.java
@@ -306,8 +306,7 @@ public class SecondProductController {
 
 
         if (isPutaway.equals(ProductEnum.IsPutaway.PUTAWAY.getState())){
-            Integer[] a1 = new Integer[productId];
-            a1[0] = productId;
+            Integer[] a1 = {productId};
             IsPutaway(a1,isPutaway);
             System.out.println(a1[0]);
         }else {
